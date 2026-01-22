d4a323fe860c37f0085baf13bf7ffc1005399414
muskanxxmehra <muskan.mehra@azalio.io>
2026-01-22 17:56:04 +0530

    Update main.tf

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -20,7 +20,7 @@ resource "aws_iam_role" "ec2_role" {
 }
 
 resource "aws_iam_instance_profile" "ec2_profile" {
-  name = "${var.environment}-ec2-profile"
+  name = "${var.environment}-ec2-profile-v2"
   role = aws_iam_role.ec2_role.name
 }
 
