8d046fd7d6b2ee6a10efda18fee09ff6de0e077f
muskanxxmehra <muskan.mehra@azalio.io>
2026-01-22 14:41:20 +0530

    Update main.tf

--- modules/ec2-app/main.tf
+++ modules/ec2-app/main.tf
@@ -1,8 +1,8 @@
 ################################################################################
-# EC2 DB Module - Oracle XE 21c via Docker Compose on Ubuntu
+# EC2 App Module - Application Server on Ubuntu
 ################################################################################
 
-resource "aws_instance" "db" {
+resource "aws_instance" "app" {
   ami                         = var.ami_id
   instance_type               = var.instance_type
   subnet_id                   = var.subnet_id
@@ -19,20 +19,28 @@ resource "aws_instance" "db" {
   }
 
   user_data = base64encode(templatefile("${path.module}/user_data.sh", {
-    db_user               = var.db_user
-    db_password           = var.db_password
-    aws_region            = var.aws_region
-    aws_access_key_id     = var.aws_access_key_id
-    aws_secret_access_key = var.aws_secret_access_key
+    db_host     = var.db_host
+    db_user     = var.db_user
+    db_password = var.db_password
+    app_port    = var.app_port
   }))
 
   tags = merge(var.tags, {
-    Name     = "${var.environment}-oracle-db-server"
-    Role     = "Database"
-    Database = "Oracle XE 21c (Docker)"
+    Name = "${var.environment}-${var.app_name}"
+    Role = "Application"
   })
 
   lifecycle {
     create_before_destroy = true
   }
+}
+
+resource "aws_eip" "app" {
+  count    = var.create_eip ? 1 : 0
+  instance = aws_instance.app.id
+  domain   = "vpc"
+
+  tags = merge(var.tags, {
+    Name = "${var.environment}-${var.app_name}-eip"
+  })
 }
