2218c2a85195240c2016249bef8b768b7cbbd374
muskanxxmehra <muskan.mehra@azalio.io>
2026-01-22 16:40:13 +0530

    Update outputs.tf

--- services/aws-app-db-oracle/outputs.tf
+++ services/aws-app-db-oracle/outputs.tf
@@ -104,3 +104,13 @@ output "oracle_sqlplus_command" {
   sensitive   = true
 }
 
+output "s3_bucket_name" {
+  description = "S3 bucket name"
+  value       = aws_s3_bucket.demo_bucket.id
+}
+
+output "s3_bucket_arn" {
+  description = "S3 bucket ARN"
+  value       = aws_s3_bucket.demo_bucket.arn
+}
+
