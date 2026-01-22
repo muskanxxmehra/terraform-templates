90d323bcc4f0cafb61686b0b0b57d4c9b6093ba4
muskanxxmehra <muskan.mehra@azalio.io>
2026-01-22 17:03:06 +0530

    Update outputs.tf

--- modules/iam/outputs.tf
+++ modules/iam/outputs.tf
@@ -1,10 +1,19 @@
-output "instance_profile_name" {
-  description = "Instance profile name"
-  value       = aws_iam_instance_profile.ec2_profile.name
-}
-
 output "role_arn" {
-  description = "IAM role ARN"
+  description = "IAM Role ARN"
   value       = aws_iam_role.ec2_role.arn
 }
 
+output "role_name" {
+  description = "IAM Role Name"
+  value       = aws_iam_role.ec2_role.name
+}
+
+output "instance_profile_name" {
+  description = "IAM Instance Profile Name"
+  value       = aws_iam_instance_profile.ec2_profile.name
+}
+
+output "instance_profile_arn" {
+  description = "IAM Instance Profile ARN"
+  value       = aws_iam_instance_profile.ec2_profile.arn
+}
