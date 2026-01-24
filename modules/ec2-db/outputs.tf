311eb6a6dff5e8af4f7eb055f820a7d3b7857b37
muskanxxmehra <muskan.mehra@azalio.io>
2026-01-25 04:18:36 +0530

    Update outputs.tf

--- modules/ec2-db/outputs.tf
+++ modules/ec2-db/outputs.tf
@@ -2,6 +2,11 @@
 # EC2 DB Module Outputs - Oracle XE via Docker
 ################################################################################
 
+# To get the EIP public IP, we need to use a data source
+data "aws_eip" "db" {
+  id = var.elastic_ip_allocation_id
+}
+
 output "instance_id" {
   description = "Instance ID of the Oracle XE database server"
   value       = aws_instance.db.id
@@ -13,13 +18,18 @@ output "private_ip" {
 }
 
 output "public_ip" {
-  description = "Public IP address of the database server"
-  value       = aws_instance.db.public_ip
+  description = "Public IP address of the database server (Elastic IP)"
+  value       = data.aws_eip.db.public_ip
 }
 
 output "public_dns" {
-  description = "Public DNS name of the database server"
-  value       = aws_instance.db.public_dns
+  description = "Public DNS name of the database server (Elastic IP)"
+  value       = data.aws_eip.db.public_dns
+}
+
+output "elastic_ip_association_id" {
+  description = "EIP association ID"
+  value       = aws_eip_association.db.id
 }
 
 output "connection_string" {
@@ -32,3 +42,12 @@ output "jdbc_url" {
   value       = "jdbc:oracle:thin:@${aws_instance.db.private_ip}:1521/XEPDB1"
 }
 
+output "connection_string_public" {
+  description = "Oracle connection string (using public EIP)"
+  value       = "${var.db_user}@${data.aws_eip.db.public_ip}:1521/XEPDB1"
+}
+
+output "jdbc_url_public" {
+  description = "JDBC connection URL (using public EIP)"
+  value       = "jdbc:oracle:thin:@${data.aws_eip.db.public_ip}:1521/XEPDB1"
+}
